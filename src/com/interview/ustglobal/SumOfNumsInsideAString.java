3b32e79ed6601727c2049b0d17851d3bad14582a
rafiduekula0009 <rafidudekula009@gmail.com>
2024-06-26 12:52:58 +0530

    UTS Global interview quest by ramjan

---
+++ src/com/interview/ustglobal/SumOfNumsInsideAString.java
@@ -0,0 +1,28 @@
+package com.interview.ustglobal;
+
+import java.util.stream.Stream;
+
+public class SumOfNumsInsideAString {
+
+	static int sum=0;
+	public static void main(String[] args) {
+
+		String input = "My 90 name 10 is 50 Ramjan";
+
+		String[] inputArray = input.split(" ");
+
+		Stream.of(inputArray).filter(SumOfNumsInsideAString::isNumber).forEach(System.out::println);
+		System.out.println("Sum=> "+sum);
+	}
+
+	public static boolean isNumber(String str) {
+
+		try {
+			Double num = Double.parseDouble(str);
+			sum+=num;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+}
