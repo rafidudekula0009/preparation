055b213349adbbcbeed2b1a7d28f68bc1dc5cfe3
rafiduekula0009 <rafidudekula009@gmail.com>
2024-07-25 16:41:33 +0530

    Tech mahindra Reverse a string with special charaters. Should only reverse the non special characters in the string.

---
+++ src/com/tech/mahindra/ReverseAStringWithSpecialChars.java
@@ -0,0 +1,45 @@
+package com.tech.mahindra;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//Should revers the string but the special char's position should remain same at same index.
+//Should revers only the non special characters
+public class ReverseAStringWithSpecialChars {
+
+	public static void main(String[] args) {
+		String spclChars = "$&";
+		String input = "Swa$pn&il"; //output: lin$pa&wS
+
+		String inputWithoutSpecialChars;
+		Map<Integer, String> spclCharIndexMap = new HashMap<Integer, String>();
+		inputWithoutSpecialChars = Stream.of(input.split("")).filter(ch -> {
+			if (!spclChars.contains(ch)) {
+				return true;
+			} else {
+				spclCharIndexMap.put(input.indexOf(ch), ch);
+				return false;
+			}
+		}).collect(Collectors.joining());
+
+		inputWithoutSpecialChars = Stream.of(inputWithoutSpecialChars.split("")).reduce("", (a, b) -> b + a);
+
+		for (Entry<Integer, String> s : spclCharIndexMap.entrySet()) {
+			inputWithoutSpecialChars = addChar(inputWithoutSpecialChars, (s.getValue()).charAt(0), s.getKey());
+		}
+
+		System.out.println(inputWithoutSpecialChars);
+	}
+
+	public static String addChar(String str, char ch, int position) {
+		int len = str.length();
+		char[] updatedArr = new char[len + 1];
+		str.getChars(0, position, updatedArr, 0);
+		updatedArr[position] = ch;
+		str.getChars(position, len, updatedArr, position + 1);
+		return new String(updatedArr);
+	}
+}
